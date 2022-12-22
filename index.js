54ec3b81f3bf476f23e4fc32605bfb8706991cb5
TheD1TZ <ItsDitz33@gmail.com>
2022-12-22 13:57:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const dotenv = require("dotenv");
-const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType, Presence } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
+  // Logs if bot is online!
   console.log(`Logged in as ${client.user.tag}!`);
+
+  //Changes the activity
   client.user.setActivity("Uptempo", {
     type: ActivityType.Listening
   })
+
+
 });
 
 
