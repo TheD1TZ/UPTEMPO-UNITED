29cabbafabc28e8c2f9bb3e6e92d667e21305482
TheD1TZ <ItsDitz33@gmail.com>
2022-12-22 13:49:29 +0100

    Works

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const dotenv = require("dotenv");
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("Uptempo", {
+    type: ActivityType.Listening
+  })
 });
 
 
