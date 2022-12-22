0f299bdd9558be62ba60ffa9e11809bb42f716ff
TheD1TZ <83147115+TheD1TZ@users.noreply.github.com>
2022-12-22 10:12:50 +0000

    Bot

---
+++ index.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.login(dotenv.config().parsed.TOKEN);
